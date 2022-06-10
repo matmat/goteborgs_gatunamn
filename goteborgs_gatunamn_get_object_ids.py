af0b08fb6b63cee87efc547b75f39032dd75253e
Mattias Mattsson <vitplister@gmail.com>
2022-06-10 20:29:38 +0200

    Get all object ids that have a value in the "Värde, gatunamn" instead of "Status - gatunamn" as before.

--- goteborgs_gatunamn_get_object_ids.py
+++ goteborgs_gatunamn_get_object_ids.py
@@ -3,45 +3,51 @@
 import requests
 import xml.etree.ElementTree as ET
 import re
-
+import sys
 
 carlotta_base_url            = "https://samlingar.goteborgsstadsmuseum.se"
-carlotta_element_search_path = "/carlotta/web/perform/element_search"
-carlotta_query_url           = carlotta_base_url + carlotta_element_search_path
+carlotta_advanced_search_path = "/carlotta/web/perform/advanced_search"
+carlotta_query_url           = carlotta_base_url + carlotta_advanced_search_path
 carlotta_object_browse_path  = "/carlotta/web/perform/object_browse"
 carlotta_object_browse_url   = carlotta_base_url + carlotta_object_browse_path
 
-status_gatunamn_dataelement = "13018275"
-status_gatunamn = ["beslutat namn, ej färdig gata",
-                   "ej fastställd",
-                   "gällande",
-                   "införlivad",
-                   "tillfälligt namn",
-                   "utgått"]
 
-operation = "EQUALS"
+ortnamn_register = "13018268"
+spe = "OBJNAMN"
+varde_gatunamn_object = "Värde, gatunamn"
+operation = "EXISTS"
 
 object_ids = list()
 
-for status in status_gatunamn:
-    resp=requests.get(carlotta_query_url, [('dataelement', status_gatunamn_dataelement), ('value_urlencoded',status), ('op', operation)])
-    doc_root=ET.fromstring(resp.text)
 
-    # Format is e.g "Ortnamn (6,515)"
-    number_of_hits = doc_root.find(".//*[@class='register_filter']").text
-    number_of_hits = int(''.join(re.findall(r'[^\D]',number_of_hits)))
+# https://samlingar.goteborgsstadsmuseum.se/carlotta/web/perform/advanced_search?register=13018268&spe_1=OBJNAM&obj_1=V%C3%A4rde%2C+gatunamn&op_1=EXISTS
+resp=requests.get(carlotta_query_url, [('register', ortnamn_register), ('spe_1', spe), ('obj_1', varde_gatunamn_object), ('op_1', operation)])
+doc_root=ET.fromstring(resp.text)
+
+# Format is e.g "Ortnamn (6,515)"
+number_of_hits = doc_root.find(".//*[@class='register_filter']").text
+number_of_hits = int(''.join(re.findall(r'[^\D]',number_of_hits)))
 
 
 #    print(status)
-    for n in range(number_of_hits):
+for n in range(number_of_hits):
 
-        resp=requests.get(carlotta_object_browse_url, [('browseEnvironment', carlotta_element_search_path + '?' + '&'.join(['dataelement=' + status_gatunamn_dataelement, 'op=' + operation, 'value=' + status])), ('detailBrowseIndex', n)],allow_redirects=False)
+    resp = requests.get(carlotta_object_browse_url, \
+      [('browseEnvironment', \
+           carlotta_advanced_search_path + '?' \
+           + '&'.join(['register=' + ortnamn_register, \
+                       'spe_1='    + spe, \
+                       'obj_1='    + varde_gatunamn_object, \
+                       'op_1='     + operation])), \
+        ('detailBrowseIndex', n)], \
+    allow_redirects=False)
 
-        object_id = resp.headers['Location'].split(';')[0].split("/")[-1]
+    object_id = resp.headers['Location'].split(';')[0].split("/")[-1]
 
-#        object_ids.add(object_id)
-        object_ids.append(object_id)
+#    object_ids.add(object_id)
+    object_ids.append(object_id)
 
-        print(object_id)
+    print(object_id)
+    print(n, object_id, file=sys.stderr)
 
-#    print("antal: ",len(object_ids))
+#print("antal: ",len(object_ids))
